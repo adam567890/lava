b54d48011926f198fd1a11e8911432dcb22357f5
adam567890 <152234246+adam567890@users.noreply.github.com>
2024-02-23 23:05:11 -0500

    Update sketch_feb14a.ino
    Added additional comments for clarity

--- sketch_feb14a.ino
+++ sketch_feb14a.ino
@@ -6,36 +6,43 @@
 // Motor A
 #define Ain1 7
 #define Ain2 6
+
 // Motor B
 #define Bin1 8
 #define Bin2 9
+
 //PWMs
 #define pwm1 10 //A
 #define pwm2 11 //B
+
 //Sensors
 #define LOX1_ADDRESS 0x30 //address for wheel side
 #define LOX2_ADDRESS 0x31 //address for other side
-// set the pins to shutdown
-#define SHT_LOX1 26 //shutdown 
-#define SHT_LOX2 27 //shutdown
+
+// set the pins to shutdown ToF
+#define SHT_LOX1 26 //shutdown ToF 1
+#define SHT_LOX2 27 //shutdown ToF 2
 #define ledint 39 //LED interrupt pin for color sensor
 
-#define blackValue 65 //Black color reading
-#define whiteValue 405 // White color reading
+#define blackValue 65 //Black color reading (constant, change for new surfaces)
+#define whiteValue 405 // White color reading (constant, change for new surfaces)
+
 float blackThreshold = (blackValue + whiteValue)/2; //threshold for line following
 double PROPORTIONAL_GAIN = 1.1; //Proportional gain for turn rate
+
 // objects for the vl53l0x laser sensor
 Adafruit_VL53L0X lox1 = Adafruit_VL53L0X();
 Adafruit_VL53L0X lox2 = Adafruit_VL53L0X();
+
 //object for color sensor
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_60MS, TCS34725_GAIN_1X);
 
-// this holds the measurement
+// this holds the measurement for ToF sensors
 VL53L0X_RangingMeasurementData_t measure1;
 VL53L0X_RangingMeasurementData_t measure2;
 
 /*
-    Reset all sensors by setting all of their XSHUT pins low for delay(10), then set all XSHUT high to bring out of reset
+    Reset all ToF sensors by setting all of their XSHUT pins low for delay(10), then set all XSHUT high to bring out of reset
     Keep sensor #1 awake by keeping XSHUT pin high
     Put all other sensors into shutdown by pulling XSHUT pins low
     Initialize sensor #1 with lox.begin(new_i2c_address) Pick any number but 0x29 and it must be under 0x7F. Going with 0x30 to 0x3F is probably OK.
@@ -70,10 +77,7 @@ void setID() {
   if(!lox2.begin(LOX2_ADDRESS, false, &Wire)) {
     Serial.println(F("Failed to boot second VL53L0X"));
     while(1);
-  }
-
-
-  
+  }  
   // Now we're ready to get readings!
 }
 
@@ -87,10 +91,12 @@ void setup() {
   pinMode(Bin2, OUTPUT);
   pinMode(pwm1, OUTPUT);
   pinMode(pwm2, OUTPUT);
+
   analogWriteFrequency(10, 100000); // Teensy 3.0 pin 3 also changes to 375 kHz
   analogWriteFrequency(11, 100000); // Teensy 3.0 pin 3 also changes to 375 kHz
   analogWriteResolution(11);  // analogWrite value 0 to 4095, or 4096 for high
-  Serial.begin(115200);
+
+  Serial.begin(115200); //open serial port for USB debugging
   
   // wait until serial port opens for native USB devices
   while (! Serial) { delay(1); }
@@ -103,19 +109,17 @@ void setup() {
     while (1);
   }
 
-  pinMode(SHT_LOX1, OUTPUT);
+  pinMode(SHT_LOX1, OUTPUT); //setting pinmode for shutdown pins for ToF
   pinMode(SHT_LOX2, OUTPUT);
 
   Serial.println(F("Shutdown pins inited..."));
 
-  digitalWrite(SHT_LOX1, LOW);
+  digitalWrite(SHT_LOX1, LOW); //Setting shutdownpins to initialize reset and address assignment process
   digitalWrite(SHT_LOX2, LOW);
 
   Serial.println(F("Both in reset mode...(pins are low)"));
-  
-  
   Serial.println(F("Starting..."));
-  setID();
+  setID(); //Call setID to assign I2C addresses to sensors
   
  
 }
@@ -124,12 +128,12 @@ void loop() {
    
    for(int i = 0; i < 70; i++)
    {
-    followLine();
+    followLine(); // follow line needs to be called continuously for given timeframe or until a given condition is met
    }
-   stopMotors();
-   read_dual_sensors();
+   stopMotors(); // make sure to call stopmotors when done to stop forward motion as adjustments will no longer be made once outside follow line
+   read_dual_sensors(); //still having trouble with this one
    //delay(100);
-   getColor();
+   getColor(); //working as intended, utilized in follow line, but called here for printout to serial
    //delay(100);
  
   
@@ -146,6 +150,7 @@ void followLine()
 
 void drive(float drive_speed, int turn_rate)
 {
+  //Set motor driver logic pins for forward motion
   digitalWrite(Ain1, LOW);
   digitalWrite(Ain2, HIGH);
   digitalWrite(Bin1, HIGH);
@@ -227,12 +232,12 @@ void getColor()
 
 
 void straight() {
-  digitalWrite(Ain1, LOW);
+  digitalWrite(Ain1, LOW); //Motor driver logic set up for forward motion on both motors
   digitalWrite(Ain2, HIGH);
   digitalWrite(Bin1, HIGH);
   digitalWrite(Bin2, LOW);
-  analogWrite(11, 656); //Set PWM to max
-  analogWrite(10, 656); //Set PWM to max
+  analogWrite(11, 2047); //Set PWM to max
+  analogWrite(10, 2047); //Set PWM to max
 }
 
 void moveBackward() {
